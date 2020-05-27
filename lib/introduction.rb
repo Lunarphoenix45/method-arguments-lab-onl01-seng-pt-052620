a4e51a3f8082db0f946f976da9c474a6bed3737e
Joshua Ortega <joshua.ortega13@gmail.com>
2020-05-27 14:22:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction(name)
-  puts "Hi, my name is #{name}."
+  puts "Hi, my name is #{name}"
 end
 def introduction_with_languager(dan, ember.js)
   puts "hi,my name is #{name} and i am learning to program in  #{language}"
